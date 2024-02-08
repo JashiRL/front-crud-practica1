ab44ed8217621cd88a1934c3c14216288a050391
LoiLoL421 <j.robleslecona@ugto.mx>
2024-02-08 09:40:53 -0600

    añadimos la vista para una card del login, usamos algunas reglas, entre otros

---
+++ components/users/ui-login.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card width="400" heigth="400" color="cyan lighten-5">
+    <v-card-title> Welcome</v-card-title>
+    <v-card-text>
+      <v-form v-model="valid">
+        <v-text-field v-model="usuario" type="text" placeholder="Escribe tu usuario" label="Usuario" />
+        <v-text-field v-model="password" type="password" placeholder="Escribe la contraseña" label="Password" />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn elevation="0" color="cyan lighten-5" @click="loginUser">
+        <v-icon color="blue lighten-4">
+          mdi-
+        </v-icon>
+        Login
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      valid: false,
+      usuario: '',
+      password: ''
+    }
+  },
+  methods: {
+    loginUser () {
+      // eslint-disable-next-line no-console
+      console.log('Press de Button')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-row class="centerClass">
+      <Nuxt />
+    </v-row>
+  </v-app>
+</template>
+
+<style scoped>
+.centerClass {
+  width: 100vw;
+  height: 100vh;
+  display: center;
+  align-items: center;
+  justify-content: center;
+  background-color: red lighten-5;
+}
+</style>
